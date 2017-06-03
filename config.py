0d13e2f0594d4599ea5cc44092f584ff5d48d8a7
yeere91 <regannn@gmail.com>
2017-06-02 22:02:50 -0400

    adding config file

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+    
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/vehicle'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
